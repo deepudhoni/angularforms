d19280e55717fb6f2736e7179c88b6eaf26050ae
deepudhoni <anudeep4183@gmail.com>
2020-02-26 15:57:19 +0530

    commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+  myform: FormGroup;
+  status:boolean;
+  check: boolean=false
+  ptn="[0-9]*"
+   constructor() { }
+   ngOnInit() {
+    this.myform = new FormGroup({
+      name: new FormControl('',[Validators.required]),
+      phone: new FormControl('',[Validators.required,Validators.pattern(this.ptn),Validators.minLength(10),Validators.maxLength(10)]),
+      address: new FormGroup({
+        street: new FormControl(),
+        city: new FormControl(),
+        zip: new FormControl('',[Validators.pattern(this.ptn),Validators.minLength(6),Validators.maxLength(6)])
+      })
+     
+  });
+   }
+   ngOnChanges(){
+     
+   }
+   
+get name() {
+  return this.myform.get('name');
+} 
+get phone()
+{
+  return this.myform.get('phone');
+}
+get zip()
+{
+  return this.myform.get("address.zip");
+}
+
+   onClickSubmit(data) {
+     
+   }
 }
